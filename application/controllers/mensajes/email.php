1e97d11254484c1c8cf41b3602a781b7e91d0ebf
cisu18 <avilasauna@gmail.com>
2014-04-16 09:33:18 -0500

    modifcacion_controller_email

--- application/controllers/mensajes/email.php
+++ application/controllers/mensajes/email.php
@@ -11,35 +11,24 @@ class email extends CI_Controller {
 	{
         $prodSinStock = $this->input->post('table_productos');
         $trabCorreo = $this->input->post('trabajadoresId');
-        $from = $this->ion_auth->user()->row()->email;
+        $from = $this->ion_auth->user()->row()->email;		    
+            
+        $para           =  "avilasauna@gmail.com";
+        $subject        =  "Reporte de Productos con Stock Minimo";
+        $msg            =  "holaaaaaaaa" ;
+        $mainheaders    =  "Content-type: text/html; charset=utf-8\r\n";
+        $mainheaders   .=  "From: dana_17e@hotmail.com";
+        $mainheaders   .=  "To: sabrina_46_6@hotmail.com";
 
-		$config = array(
-        'protocol' => 'smtp',
-        'smtp_host' => 'mail.clmdevelopers.com',
-        'smtp_port' => 25,
-        'smtp_user' => 'contacto@clmdevelopers.com', 
-        'smtp_pass' => 'contactclmdev',  
-        'mailtype' => 'html',       
-    );
+        $resultado = mail ($para, $subject, $msg, $mainheaders);
 
+        if($resultado){
+           echo 'Tu mensaje ha sido enviado!';
+            }        
+        else
+            echo 'Error!';
 
-    $this->load->library('email',$config);
-    $this->email->initialize($config);
-
-    $this->email->from($from);
-    $this->email->to($trabCorreo);
-    $this->email->cc('avilasauna@gmail.com');
-
-    $this->email->subject('Reporte de Productos con Stock Minimo');
-    $this->email->message($prodSinStock);
-
-    if ($this->email->send()) 
-
-        echo 'Tu mensaje ha sido enviado!';
-    else 
-        echo 'Error al enviar el mensaje!';
-					
-	}
+    }
 
 
 }
